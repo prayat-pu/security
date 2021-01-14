6f4e6c06862e2f0c6cee02fdc2194bab45066925
prayat-pu <bosssds65@gmail.com>
2021-01-15 06:22:02 +0700

    add portscanning python file

---
+++ portscanning.py
@@ -0,0 +1,26 @@
+import pocket
+import termcolor 
+
+def scan(target,ports):
+    for port in range(1,ports):
+        scan_port(target,port)
+
+def scan_port(ip_address,port):
+    try:
+        sock = socket.socket()
+        sock.connect((ip_address,port))
+        print("[*] Port Opened "+ str(port))
+        sock.close()
+    except:
+        pass
+
+targets = input('[*] Enter Targets To Scan(split them by ,): ')
+ports = int(input('[*] Enter How Many Ports You Want To Scan: '))
+
+if ',' in targets:
+    print(termcolor.colored('[*] Scanning Multiple Targets','green'))
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(' '),ports)
+else:
+    scan(targets,ports)
+
